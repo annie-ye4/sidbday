7f668a18e94b8f4e56f174972817493335853b4b
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-02-22 02:32:13 -0500

    TODO: fix spacing of home page

---
+++ JS/Start.js
@@ -0,0 +1,25 @@
+// shows hint when hover
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+// checks if the password is correct
+document.querySelector("button").addEventListener("click", function () {
+  var password = document.querySelector("input").value;
+  if (password === "9102023") {
+    window.location.replace("ConnectionPage.html");
+  } else {
+    var rndNamber = Math.floor(Math.random() * 3) + 1;
+    switch (rndNamber) {
+      case 1:
+        alert("wrong :( </3");
+        break;
+      case 2:
+        alert("this is embarrassing for you... please lock in sid");
+        break;
+      case 3:
+        alert("it's not that hard bae");
+        break;
+    }
+  }
+});
